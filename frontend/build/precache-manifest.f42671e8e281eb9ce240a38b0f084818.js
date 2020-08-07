b31cce852e769421ccd9addc7ab8c66771a3ce45
Deep Bratt Mondal <deepbratt@gmail.com>
2020-08-08 02:01:44 +0530

    final frontend push

---
+++ frontend/build/precache-manifest.f42671e8e281eb9ce240a38b0f084818.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15ca9b55233bc3542da4df93f803a618",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc1924431596074703e4",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "8631d6590d7b29585f18",
+    "url": "/static/css/main.5d2a0eec.chunk.css"
+  },
+  {
+    "revision": "cc1924431596074703e4",
+    "url": "/static/js/2.998db62b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.998db62b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8631d6590d7b29585f18",
+    "url": "/static/js/main.2da9a65c.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
--- frontend/src/utils/gen.utils.js
+++ frontend/src/utils/gen.utils.js
@@ -1,6 +1,8 @@
-// export const API_URL = "https://urlshortner07.herokuapp.com/api/urls";
-export const API_URL = "http://localhost:4000/api/urls";
-export const SITE_URL = "http://localhost:3000/";
+export const API_URL = "https://urlshortener05.herokuapp.com/api/urls";
+// export const API_URL = "http://localhost:4000/api/urls";
+// export const SITE_URL = "http://localhost:3000/";
+export const SITE_URL =
+  "http://testdeploy5.s3-website.ap-south-1.amazonaws.com/";
 export const isUrlValid = (input) => {
   var res = input.match(
     /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/g
